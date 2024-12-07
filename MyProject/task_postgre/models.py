90e6697617bd4bad133260ced1afdd9b8040e952
Alexey A. Tsukanov <a.a.tsukanov@yandex.ru>
2024-12-07 14:03:07 +0300

    добавлено решение 19.6 + операции решения записал в файл Решения.txt + добавил еще две модели из ДЗ 19.1, смигрировал, заполнил.

--- MyProject/task_postgre/models.py
+++ MyProject/task_postgre/models.py
@@ -5,3 +5,28 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+# новые добавим из 19.1:
+class Buyer(models.Model):
+    name = models.CharField(max_length=64)
+    balance = models.DecimalField(max_digits=12, decimal_places=2)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    cost = models.DecimalField(max_digits=12, decimal_places=2)
+    size = models.DecimalField(max_digits=12, decimal_places=3)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    # многие-ко-многим:
+    buyer = models.ManyToManyField(Buyer, related_name='game')
+
+    def __str__(self):
+        return self.title
